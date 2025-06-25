e4cd0661e15d83dfae3dacfad8afc68c4ee8a39e
KAIJIN2525 <emememdavid086@gmail.com>
2025-06-25 22:37:19 +0100

    Implement Cocktails Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import gsap from "gsap";
 
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
 gsap.registerPlugin( ScrollTrigger, SplitText);
 const App = () => {
     return (
         <main>
             <Navbar />
             <Hero />
+            <Cocktails />
         </main>
     );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -65,7 +65,7 @@ const Hero = () => {
     return (
         <>
             <section id="hero" className="noisy">
-                <h1 className='title'>NEGRONI</h1>
+                <h1 className='title'>DAIQUIRI</h1>
                 <img src="/images/hero-left-leaf.png" alt="left leaf" className="left-leaf" />
                 <img src="/images/hero-right-leaf.png" alt="right leaf" className="right-leaf" />
 
